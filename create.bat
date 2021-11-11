9707919a13875290aff8ccc27b4b75281d3d4eae
wu58430 <wu58430@126.com>
2021-11-12 00:49:44 +0800

    first commit

--- create.bat
+++ create.bat
@@ -1,12 +1,6 @@
 git init
-git config --global http.proxy http://127.0.0.1:1080 
 git add *
 git commit -m "first commit"
-git branch -M main  
-git remote add origin https://github.com/wu58430/smartcar.git
+git branch -M main
+git remote add origin https://github.com/wu58430/smart_car.git
 git push -u origin main
-PAUSE
-
-
-
-
