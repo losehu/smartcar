b20d1586a17b0ea25027090fa67821ad06e4af56
wu58430 <wu58430@126.com>
2021-11-12 01:14:43 +0800

    second change

--- upload.bat
+++ upload.bat
@@ -1,9 +1,9 @@
-git clone  git@github.com:wu58430/smartcar.git
+git pull https://github.com/wu58430/
 git config --global http.proxy http://127.0.0.1:1080 
 git add *
 git commit -m "second change"
 git branch -M main 
-git push -u origin main
+git push https://github.com/wu58430/
 PAUSE
 
 
